328b95cc5436420cfa3d6405afc53332d69dc777
Daniel Baldwin <pedee@george-2.local>
2019-01-22 09:40:56 -0800

    internaized the getallheaders method so you never get an error if it's not available.

--- src/App.php
+++ src/App.php
@@ -5,7 +5,7 @@ namespace True;
  *
  * @package True Framework
  * @author Daniel Baldwin
- * @version 1.3.1
+ * @version 1.3.2
  */
 class App
 {
@@ -405,7 +405,7 @@ class App
 					$callbackArgs->get = (object)$_GET;
 				}
 
-				$callbackArgs->headers = (object)getallheaders();
+				$callbackArgs->headers = (object) $this->getallheaders();
 				if (is_string($callable)) {
 					$this->includeController($callable, $callbackArgs, $customControllerPath);
 				}
@@ -594,5 +594,43 @@ class App
 			return 'yes';
 		}
 		return 'no';
+	}
+
+	/**
+   * Get all HTTP header key/values as an associative array for the current request.
+   * Written by ralouphie - https://github.com/ralouphie
+   *
+   * @return string[string] The HTTP header key/value pairs.
+   */
+	protected function getallheaders()
+	{
+		$headers = array();
+		$copy_server = array(
+			'CONTENT_TYPE'   => 'Content-Type',
+			'CONTENT_LENGTH' => 'Content-Length',
+			'CONTENT_MD5'    => 'Content-Md5',
+		);
+		foreach ($_SERVER as $key => $value) {
+			if (substr($key, 0, 5) === 'HTTP_') {
+				 $key = substr($key, 5);
+				 if (!isset($copy_server[$key]) || !isset($_SERVER[$key])) {
+					  $key = str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', $key))));
+					  $headers[$key] = $value;
+				 }
+			} elseif (isset($copy_server[$key])) {
+				 $headers[$copy_server[$key]] = $value;
+			}
+		}
+		if (!isset($headers['Authorization'])) {
+			if (isset($_SERVER['REDIRECT_HTTP_AUTHORIZATION'])) {
+				 $headers['Authorization'] = $_SERVER['REDIRECT_HTTP_AUTHORIZATION'];
+			} elseif (isset($_SERVER['PHP_AUTH_USER'])) {
+				 $basic_pass = isset($_SERVER['PHP_AUTH_PW']) ? $_SERVER['PHP_AUTH_PW'] : '';
+				 $headers['Authorization'] = 'Basic ' . base64_encode($_SERVER['PHP_AUTH_USER'] . ':' . $basic_pass);
+			} elseif (isset($_SERVER['PHP_AUTH_DIGEST'])) {
+				 $headers['Authorization'] = $_SERVER['PHP_AUTH_DIGEST'];
+			}
+		}
+		return $headers;
 	}
 }
