0e616403b4c6af46cc4c834797485ba561e7f8a8
Daniel Baldwin <pedee@Mark.PK5001Z>
2019-04-10 11:38:28 -0700

    update the Auth class

--- src/Auth.php
+++ src/Auth.php
@@ -4,46 +4,39 @@ namespace True;
 /**
  * Auth class for authenticating api calls
 
- $Auth->
  *
- * @package default
- * @author 
+ * @package True Framework
+ * @author Daniel Baldwin
  */
 class Auth
 {
     /**
-     * undocumented function
+     * request token
      *
      * @param string $randomChars - at least 10 letters and numbers
      * @return void
      * @author Daniel Baldwin - danb@truecastdesign.com
      **/
-    public function requestToken($randomChars)
+    public function requestToken()
     {
-    	if(empty($randomChars))
-    	{
-    		trigger_error('No random characters provided', 256);
-    		return false;
-    	}	
+        # get salt
+        $fp = fopen('/dev/urandom', 'r');
+        $salt = base64_encode(fread($fp, 16));
+        fclose($fp);
 
-    	# get salt
-    	$fp = fopen('/dev/urandom', 'r');
-		$salt = base64_encode(fread($fp, 16));
-		fclose($fp);
+        # create token
+        $token = hash("sha256", $salt);
 
-		# create token
-    	$token = hash("sha256", $randomChars.$salt);
-
-    	# save token to file
-    	if(file_put_contents(BP.'/vendor/truecastdesign/true/auth_tokens', $token."\n", FILE_APPEND | LOCK_EX) === false)
-    	{
-    		trigger_error("Token could not be written to a file!", 256);
-    		return "Token could not be written to a file!";
-    	}
-    	else
-    	{
-    		return $token;	
-		}	    	
+        # save token to file
+        if(file_put_contents(BP.'/vendor/truecastdesign/true/auth_tokens', $token."\n", FILE_APPEND | LOCK_EX) === false)
+        {
+            trigger_error("Token could not be written to a file!", 256);
+            return "Token could not be written to a file!";
+        }
+        else
+        {
+            return $token;  
+        }           
     }
 
     /**
@@ -55,15 +48,15 @@ class Auth
      **/
     public function revokeToken($token)
     {
-    	$contents = file_get_contents(BP.'/vendor/truecastdesign/true/auth_tokens');
+        $contents = file_get_contents(BP.'/vendor/truecastdesign/true/auth_tokens');
 
-    	$updatedContents = str_replace($token."\n", '', $contents);
+        $updatedContents = str_replace($token."\n", '', $contents);
 
-    	if(file_put_contents(BP.'/vendor/truecastdesign/true/auth_tokens', $updatedContents, LOCK_EX) === false)
-    	{
-    		trigger_error("Token could not be revoked. Could not write to file!", 256);
-    		return "Token could not be revoked. Could not write to file!";
-    	}
+        if(file_put_contents(BP.'/vendor/truecastdesign/true/auth_tokens', $updatedContents, LOCK_EX) === false)
+        {
+            trigger_error("Token could not be revoked. Could not write to file!", 256);
+            return "Token could not be revoked. Could not write to file!";
+        }
     }
 
     /**
@@ -75,9 +68,50 @@ class Auth
      **/
     public function checkToken($token)
     {
-		if( strpos(file_get_contents(BP.'/vendor/truecastdesign/true/auth_tokens'), $token) !== false)
-			return true;
-		else
-			return false;
+        if( strpos(file_get_contents(BP.'/vendor/truecastdesign/true/auth_tokens'), $token) !== false)
+            return true;
+        else
+            return false;
+    }
+
+    /**
+     * authenticate the user
+     * Note: allow to pass origen domain to limit to local or certain domains or IPs
+     *
+     * Use in the controller file like this:
+     * if ($App->auth->authenticate(['type'=>'bearer'])) {
+     *  # success
+     * } else {
+     *  # failed
+     * }
+     * @param  array  $params   ['type'=>'bearer']
+     * @param  function $callbackSuccessful the function that is called if authenticate passes
+     * @param  function $callbackFail the function that is called if authenticate fails
+     * @return null 
+     */
+    public function authenticate(array $params)
+    {
+        extract($params);
+        
+        if (isset($type)) {
+            switch ($type) {
+                case 'bearer':
+                    $headers = getallheaders();
+                    if (isset($headers['Authorization'])) {
+                        $token = str_replace('Bearer ','',$headers['Authorization']);
+
+                        if (!empty($token) and $this->checkToken($token)) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    } else {
+                        return false;
+                    }
+                break;
+                default:
+                    return false;
+            }
+        }
     }
 }
