313f5baf63f8ece8792c1b12d8a356276ea16d18
Daniel Baldwin <zqwerty1@yahoo.com>
2019-08-01 19:56:47 -0700

    fix files array in request

--- src/App.php
+++ src/App.php
@@ -548,7 +548,7 @@ class App
 		}
 
 		if (isset($_FILES)) {
-			$request->files = static::parseUploadedFiles($_FILES);
+			$request->files = $_FILES;
 		}
 
 		return $request;		
@@ -622,52 +622,6 @@ class App
 
 		echo $body;
 	}
-
-	/**
-     * Parse a non-normalized, i.e. $_FILES superglobal, tree of uploaded file data.
-     *
-     * @param array $uploadedFiles The non-normalized tree of uploaded file data.
-     *
-     * @return array A normalized tree of UploadedFile instances.
-     */
-    private static function parseUploadedFiles(array $uploadedFiles)
-    {
-        $parsed = [];
-        foreach ($uploadedFiles as $field => $uploadedFile) {
-            if (!isset($uploadedFile['error'])) {
-                if (is_array($uploadedFile)) {
-                    $parsed[$field] = static::parseUploadedFiles($uploadedFile);
-                }
-                continue;
-            }
-
-            $parsed[$field] = [];
-            if (!is_array($uploadedFile['error'])) {
-                $parsed[$field] = new static(
-                    $uploadedFile['tmp_name'],
-                    isset($uploadedFile['name']) ? $uploadedFile['name'] : null,
-                    isset($uploadedFile['type']) ? $uploadedFile['type'] : null,
-                    isset($uploadedFile['size']) ? $uploadedFile['size'] : null,
-                    $uploadedFile['error'],
-                    true
-                );
-            } else {
-                $subArray = [];
-                foreach ($uploadedFile['error'] as $fileIdx => $error) {
-                    // normalise subarray and re-parse to move the input's keyname up a level
-                    $subArray[$fileIdx]['name'] = $uploadedFile['name'][$fileIdx];
-                    $subArray[$fileIdx]['type'] = $uploadedFile['type'][$fileIdx];
-                    $subArray[$fileIdx]['tmp_name'] = $uploadedFile['tmp_name'][$fileIdx];
-                    $subArray[$fileIdx]['error'] = $uploadedFile['error'][$fileIdx];
-                    $subArray[$fileIdx]['size'] = $uploadedFile['size'][$fileIdx];
-
-                    $parsed[$field] = static::parseUploadedFiles($subArray);
-                }
-            }
-        }
-
-        return $parsed;
-    }
 
 	/**
 	 * [includeController description]
