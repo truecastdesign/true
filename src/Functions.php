0d094393a8a6c398a37efc86f1fc5b7e0e793754
Daniel Baldwin <zqwerty1@yahoo.com>
2019-08-13 15:57:28 -0700

    Fix length of gentoken method

--- src/Functions.php
+++ src/Functions.php
@@ -6,7 +6,7 @@ namespace True;
  *
  * @package True Framework
  * @author Daniel Baldwin
- * @version 1.6.0
+ * @version 1.6.1
  */
 class Functions
 {
@@ -22,11 +22,11 @@ class Functions
 		**/
 	public static function encrypt($str, $key)
 	{
-			$ivlen = openssl_cipher_iv_length($cipher="AES-128-CBC");
-			$iv = openssl_random_pseudo_bytes($ivlen);
-			$ciphertext_raw = openssl_encrypt($str, $cipher, $key, $options=OPENSSL_RAW_DATA, $iv);
-			$hmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary=true);
-			return base64_encode( $iv.$hmac.$ciphertext_raw );
+		$ivlen = openssl_cipher_iv_length($cipher="AES-128-CBC");
+		$iv = openssl_random_pseudo_bytes($ivlen);
+		$ciphertext_raw = openssl_encrypt($str, $cipher, $key, $options=OPENSSL_RAW_DATA, $iv);
+		$hmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary=true);
+		return base64_encode( $iv.$hmac.$ciphertext_raw );
 	}
 
 	/**
@@ -39,17 +39,17 @@ class Functions
 		**/
 	public static function decrypt($str, $key)
 	{
-			$c = base64_decode($str);
-			$ivlen = openssl_cipher_iv_length($cipher="AES-128-CBC");
-			$iv = substr($c, 0, $ivlen);
-			$hmac = substr($c, $ivlen, $sha2len=32);
-			$ciphertext_raw = substr($c, $ivlen+$sha2len);
-			$original_plaintext = openssl_decrypt($ciphertext_raw, $cipher, $key, $options=OPENSSL_RAW_DATA, $iv);
-			$calcmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary=true);
-			if (hash_equals($hmac, $calcmac))//PHP 5.6+ timing attack safe comparison
-			{
-				return $original_plaintext;
-			}
+		$c = base64_decode($str);
+		$ivlen = openssl_cipher_iv_length($cipher="AES-128-CBC");
+		$iv = substr($c, 0, $ivlen);
+		$hmac = substr($c, $ivlen, $sha2len=32);
+		$ciphertext_raw = substr($c, $ivlen+$sha2len);
+		$original_plaintext = openssl_decrypt($ciphertext_raw, $cipher, $key, $options=OPENSSL_RAW_DATA, $iv);
+		$calcmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary=true);
+		if (hash_equals($hmac, $calcmac))//PHP 5.6+ timing attack safe comparison
+		{
+			return $original_plaintext;
+		}
 	}
 
 	/**
@@ -61,7 +61,7 @@ class Functions
 		**/
 	public static function genToken($length = 64)
 	{
-			return bin2hex(openssl_random_pseudo_bytes($length));
+		return substr(bin2hex(openssl_random_pseudo_bytes($length)), 0, $length);
 	}
 
 	/**
@@ -72,7 +72,7 @@ class Functions
 		**/
 	public static function host()
 	{
-			return $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'];
+		return $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'];
 	}
 
 	/**
