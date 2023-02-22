467a1c8ab17269c8cf3c365d5a8fb547e81681a4
Daniel Baldwin <danielbaldwin@gmail.com>
2023-02-22 10:26:28 -0800

    update articles schema

--- src/schemaTypes/Article.php
+++ src/schemaTypes/Article.php
@@ -2,6 +2,11 @@
 
 namespace True\schemaTypes;
 
+/**
+ * Schema for articles
+ * 
+ * @version 1.0.1
+ */
 class Article
 {
 	private $structure = [];
@@ -12,6 +17,24 @@ class Article
 	 * title: The title of the article
 	 * datePublished: yyyy-mm-dd
 	 * dateModified: yyyy-mm-dd
+	 * alternativeHeadline: A subtitle of the article or opening line.
+	 * image: An image of the item. This can be a URL or a fully described ImageObject. https://schema.org/ImageObject
+	 * ImageObject: ['contentUrl'=>'http', 'width'=>234, 'height'=>456, 'caption'=>'text', 'author'=>'Name', 'contentLocation'=>'City, State, Country', 'datePublished'=>'2012-01-01', 'description'=>'What the photo is about.', 'name'=>'Title of image']
+	 * 
+	 * author: Organization ['type'=>'Organization', 'name'=>'Org Name', 'address'=>'123 Main St.', 'logo'=>'url', 'telephone'=>'541-', 'slogan'=>'text']  or Person ['type'=>'Person', 'name'=>'text', 'email'=>'text', 'image'=>'url'] objects
+	 * 
+	 * award: An award won by or for this item.
+	 * editor: Person ['type'=>'Person', 'name'=>'text', 'email'=>'text', 'telephone'=>'541-', 'image'=>'url'] object
+	 * 
+	 * publisher: Organization or Person object ['type'=>'Organization', 'name'=>'Org Name', 'address'=>'123 Main St.', 'logo'=>'url', 'telephone'=>'541-', 'slogan'=>'text']
+	 * genre: text or url, Genre of the creative work, broadcast channel or group. https://vocab.getty.edu/ example: http://vocab.getty.edu/aat/300021143
+	 * keywords: keywords list are typically delimited by commas
+	 * wordcount: 56 - Integer - The number of words in the text of the Article.
+	 * url: http://
+	 * description: text
+	 * articleBody: full text
+	 * pageType: AmpStory, ArchiveComponent, Article, Atlas, Blog, Book, Chapter, Claim, Clip, Collection, ComicStory, Comment, Conversation, Course, CreativeWorkSeason, CreativeWorkSeries, DataCatalog, Dataset, DefinedTermSet, Diet, DigitalDocument, Drawing, EducationalOccupationalCredential, Episode, ExercisePlan, Game, Guide, HowTo, HowToDirection, HowToSection, HowToStep, HowToTip, HyperToc, HyperTocEntry, LearningResource, Legislation, Manuscript, Map, MathSolver, MediaObject, MediaReviewItem, Menu, MenuSection, Message, Movie, MusicComposition, MusicPlaylist, MusicRecording, Painting, Photograph, Play, Poster, PublicationIssue, PublicationVolume, Quotation, Review, Sculpture, SheetMusic, ShortStory, SoftwareApplication, SoftwareSourceCode, SpecialAnnouncement, Statement, TVSeason, TVSeries, Thesis, VisualArtwork, WebContent, WebPage, WebPageElement, WebSite
+	 * 
 	 *
 	 * @param object $info
 	 * @return void
@@ -40,6 +63,9 @@ class Article
 
 		if (isset($info->author) and !empty($info->author))
 			$data['author'] = $info->author;
+			
+		if (isset($info->author) and !empty($info->author))
+			$data['author'] = $info->author;
 
 		if (isset($info->award) and !empty($info->award))
 			$data['award'] = $info->award;
@@ -64,6 +90,9 @@ class Article
 
 		if (isset($info->articleBody) and !empty($info->articleBody))
 			$data['articleBody'] = $info->articleBody;
+
+		if (isset($info->pageType) and !empty($info->pageType) and isset($info->url) and !empty($info->url))
+			$data['mainEntityOfPage'] = ['@type'=>$info->pageType, '@id'=>$info->url, 'url'=>$info->url];
 
 		$this->structure = $data;
 	}
